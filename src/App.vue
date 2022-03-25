a204c974d57d35ee1294425aa6c6093e1c438c2d
shybili <2586468991@qq.com>
2022-03-25 23:22:13 +0800

    第二次提价

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
-  
+    <demo  @cw="says" msg="你好啊"></demo>
+   
 </template>
+<script>
+import demo from './components/demo.vue'
+export default ({
+    name:'App',
+    components:{
+       demo 
+    },
+    setup() {
+        function says(value){
+            alert(`你好啊，我的大哥${value}`)
+        }
 
-<style lang="scss">
+
+        return {
+        says
+        }
+    },
+    
+})
+</script>
+
+<style >
 
 </style>
---
+++ src/components/demo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>我是demo组件</h2>
+        <h2>{{msg}}</h2>
+        <button @click="text">点击按钮</button>
+    </div>
+</template>
+<script>
+export default ({
+    name:'demo',
+    props:['msg'],
+    setup(props,contexts) {
+        console.log(props);
+
+        function text(){
+            contexts.emit('cw',8888);
+        }
+
+        return {
+        text
+        }
+    },
+   
+})
+</script>
+
+<style >
+
+</style>
